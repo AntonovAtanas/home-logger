1e4a5296469ad21f9133f34897df23b19207814b
AntonovAtanas <114076833+AntonovAtanas@users.noreply.github.com>
2025-12-28 15:43:50 +0200

    migrate swc core version

--- apps/home-logger/src/app/app.component.ts
+++ apps/home-logger/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, ButtonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
