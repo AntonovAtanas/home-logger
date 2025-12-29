fb7d8ea17af95a84e4985987ea76402edb20a471
AntonovAtanas <114076833+AntonovAtanas@users.noreply.github.com>
2025-12-29 21:06:15 +0200

    create initial main menu

--- apps/home-logger/src/app/shared/components/main-menu/main-menu.component.ts
+++ apps/home-logger/src/app/shared/components/main-menu/main-menu.component.ts
@@ -1,83 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { BadgeModule } from 'primeng/badge';
 import { PanelMenu } from 'primeng/panelmenu';
-import { Ripple } from 'primeng/ripple';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss'],
-  imports: [PanelMenu, BadgeModule, Ripple, CommonModule],
+  imports: [PanelMenu, CommonModule],
 })
 export class MainMenuComponent {
-  // Component logic goes here
-  items: MenuItem[] = [];
-
-  ngOnInit() {
-    this.items = [
-      {
-        label: 'Mail',
-        icon: 'pi pi-envelope',
-        badge: '5',
-        items: [
-          {
-            label: 'Compose',
-            icon: 'pi pi-file-edit',
-            shortcut: '⌘+N',
-          },
-          {
-            label: 'Inbox',
-            icon: 'pi pi-inbox',
-            badge: '5',
-          },
-          {
-            label: 'Sent',
-            icon: 'pi pi-send',
-            shortcut: '⌘+S',
-          },
-          {
-            label: 'Trash',
-            icon: 'pi pi-trash',
-            shortcut: '⌘+T',
-          },
-        ],
-      },
-      {
-        label: 'Reports',
-        icon: 'pi pi-chart-bar',
-        shortcut: '⌘+R',
-        items: [
-          {
-            label: 'Sales',
-            icon: 'pi pi-chart-line',
-            badge: '3',
-          },
-          {
-            label: 'Products',
-            icon: 'pi pi-list',
-            badge: '6',
-          },
-        ],
-      },
-      {
-        label: 'Profile',
-        icon: 'pi pi-user',
-        shortcut: '⌘+W',
-        items: [
-          {
-            label: 'Settings',
-            icon: 'pi pi-cog',
-            shortcut: '⌘+O',
-          },
-          {
-            label: 'Privacy',
-            icon: 'pi pi-shield',
-            shortcut: '⌘+P',
-          },
-        ],
-      },
-    ];
-  }
+  items: MenuItem[] = [
+    {
+      label: 'Dashboard',
+      icon: 'pi pi-chart-bar',
+    },
+    {
+      label: 'Finances',
+      icon: 'pi pi-wallet',
+    },
+  ];
 }
