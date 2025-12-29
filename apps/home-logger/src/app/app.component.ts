85280c8daca86e93dcfbab5948721f0af16e3e47
AntonovAtanas <114076833+AntonovAtanas@users.noreply.github.com>
2025-12-29 21:05:42 +0200

    add main layout

--- apps/home-logger/src/app/app.component.ts
+++ apps/home-logger/src/app/app.component.ts
@@ -1,32 +1,12 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { SelectButtonModule } from 'primeng/selectbutton';
 import { MainMenuComponent } from './shared/components/main-menu/main-menu.component';
 
 @Component({
-  imports: [
-    RouterModule,
-    ButtonModule,
-    SelectButtonModule,
-    FormsModule,
-    MainMenuComponent,
-  ],
+  imports: [RouterModule, MainMenuComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   protected title = 'home-logger';
-
-  value1!: string;
-
-  value2: string = 'Beginner';
-
-  value3: string = 'Expert';
-
-  options: any[] = [
-    { label: 'Beginner', value: 'Beginner' },
-    { label: 'Expert', value: 'Expert' },
-  ];
 }
