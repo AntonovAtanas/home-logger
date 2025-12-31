229a936ee5184ae494c4e413b5c56ab0e509119f
AntonovAtanas <114076833+AntonovAtanas@users.noreply.github.com>
2025-12-31 20:41:23 +0200

    add initial dialog info

--- apps/home-logger/src/app/features/dashboard/add-property-dialog/add-property-dialog.component.ts
+++ apps/home-logger/src/app/features/dashboard/add-property-dialog/add-property-dialog.component.ts
@@ -2,11 +2,13 @@ import { Component, input, output } from '@angular/core';
 import { Dialog } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabel } from 'primeng/floatlabel';
+import { ProgressBar } from 'primeng/progressbar';
 
 @Component({
   selector: 'app-add-property-dialog',
   templateUrl: './add-property-dialog.component.html',
-  imports: [Dialog, ButtonModule, InputTextModule],
+  imports: [Dialog, ButtonModule, InputTextModule, FloatLabel, ProgressBar],
 })
 export class AddPropertyDialogComponent {
   visible = input<boolean>(false);
