4c2b67a6fbc9866b6f57f0c05bf5108bc194f559
AntonovAtanas <114076833+AntonovAtanas@users.noreply.github.com>
2026-01-10 14:29:43 +0200

    bind form controls with signal form

--- apps/home-logger/src/app/features/dashboard/add-property-dialog/add-construction-details/add-construction-details.component.ts
+++ apps/home-logger/src/app/features/dashboard/add-property-dialog/add-construction-details/add-construction-details.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Select } from 'primeng/select';
 import { DatePickerModule } from 'primeng/datepicker';
 import {
   propertyConstructionPhases,
   propertyConstructionStatuses,
 } from '../../../../consts/consts/property-consts';
+import { Field, FieldTree } from '@angular/forms/signals';
+import { Property } from 'apps/home-logger/src/app/consts/consts/property';
 
 @Component({
   selector: 'app-add-construction-details',
   templateUrl: './add-construction-details.component.html',
-  imports: [Select, DatePickerModule],
+  imports: [Select, DatePickerModule, Field],
 })
 export class AddConstructionDetailsComponent {
+  @Input() propertyForm!: FieldTree<Property>;
+
   readonly PropertyConstructionPhases = propertyConstructionPhases;
   readonly PropertyConstructionStatuses = propertyConstructionStatuses;
 }
--- apps/home-logger/src/app/features/dashboard/add-property-dialog/add-property-finances/add-property-finances.component.ts
+++ apps/home-logger/src/app/features/dashboard/add-property-dialog/add-property-finances/add-property-finances.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { propertyPaymentStructures } from 'apps/home-logger/src/app/consts/consts/property-consts';
+import { Component, Input } from '@angular/core';
+import { Property } from 'apps/home-logger/src/app/consts/consts/property';
+import {
+  PropertyPaymentStructure,
+  propertyPaymentStructures,
+} from 'apps/home-logger/src/app/consts/consts/property-consts';
+import { Field, FieldTree } from '@angular/forms/signals';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { Select } from 'primeng/select';
 
 @Component({
   selector: 'app-add-property-finances',
   templateUrl: './add-property-finances.component.html',
-  imports: [Select, InputNumberModule],
+  imports: [Select, InputNumberModule, Field],
 })
 export class AddPropertyFinancesComponent {
+  @Input() propertyForm!: FieldTree<Property>;
+
+  readonly PropertyPaymentStructureEnum = PropertyPaymentStructure;
   readonly PropertyPaymentStructures = propertyPaymentStructures;
 }
